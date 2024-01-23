9aec22b6589a21d7e259ed4bf42ce2473155d482
Ishan Pandya <ishanpandya10@gmail.com>
2024-01-23 12:25:21 +0000

    Added some documentation

--- src/main/java/com/practice/springsec/controller/HelloController.java
+++ src/main/java/com/practice/springsec/controller/HelloController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloController {
-    @PreAuthorize(value = "hasRole('User')")
+    @PreAuthorize(value = "hasRole('User')") //SpEL from object SecurityExpressionRoot
     @GetMapping(value = "/hello1")
     public String hello1(){
         System.out.println(SecurityContextHolder.getContext());
         return "Hello1";
     }
 
-    @PreAuthorize(value = "hasRole('User')")
+    @PreAuthorize(value = "hasRole('User')") //SpEL from object SecurityExpressionRoot
     @GetMapping(value = "/hello2")
     public String hello2(){
         System.out.println(SecurityContextHolder.getContext());
